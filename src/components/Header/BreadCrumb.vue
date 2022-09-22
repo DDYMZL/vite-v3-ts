4fd58c155f9ae6497e64cd6f6a0224488ac1313f
ziyou <874928519@qq.com>
2022-09-22 17:52:02 +0800

    feat: 面包屑功能初步完成

--- src/components/Header/BreadCrumb.vue
+++ src/components/Header/BreadCrumb.vue
@@ -1,13 +1,25 @@
 <template>
   <el-breadcrumb :separator-icon="ArrowRight">
-    <el-breadcrumb-item :to="{ path: '/' }"><span class="breadcrumb-text">统筹管理</span></el-breadcrumb-item>
-    <el-breadcrumb-item><span class="breadcrumb-text">业务管理</span></el-breadcrumb-item>
-    <el-breadcrumb-item><span class="breadcrumb-text">供水</span></el-breadcrumb-item>
+    <el-breadcrumb-item v-for="item in breadcrumbList" :key="item.path">
+      <span class="breadcrumb-text">{{ item.meta.title }}</span>
+    </el-breadcrumb-item>
   </el-breadcrumb>
 </template>
 
 <script lang="ts" setup>
-import { ArrowRight } from '@element-plus/icons-vue'
+import { ArrowRight } from '@element-plus/icons-vue';
+import { useRoute } from 'vue-router';
+import { watchEffect, ref } from "vue";
+
+import type { RouteRecordNormalized } from 'vue-router';
+
+const route = useRoute()
+
+const breadcrumbList = ref<RouteRecordNormalized[]>([])
+
+watchEffect(() => {
+  breadcrumbList.value = route.matched.filter(item => item.meta && item.meta.title);
+})
 </script>
 
 <style scoped>
